f27d77b976684111770ee627af2c17995828ac46
midCenturyDude7 <mgriffes@gmail.com>
2022-04-10 12:31:31 -0700

    Working with next-connect to create api routes

--- pages/api/verbs.js
+++ pages/api/verbs.js
@@ -1,4 +1,15 @@
-function handler(req, res) {
+import nextConnect from "next-connect";
+
+const handler = nextConnect({
+  onNoMatch: (req, res) => res.status(404).json({message: 'not found'})
+})
+.get((req, res) => res.status(200).json({message: 'GET!'}))
+.post((req, res) => res.status(200).json({message: 'POST!'}))
+.patch((req, res) => res.status(200).json({message: 'PATCH!'}))
+.put((req, res) => res.status(200).json({message: 'PUT!'}))
+.delete((req, res) => res.status(200).json({message: 'DELETE!'}))
+
+function oldHandler(req, res) {
   const {method} = req
 
   if (method === 'GET') {
