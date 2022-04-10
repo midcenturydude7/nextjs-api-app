534a499fc6d846fcb02eb7cb20e142d34950d7b7
midCenturyDude7 <mgriffes@gmail.com>
2022-04-10 12:02:50 -0700

    Initial commit / app config

---
+++ pages/api/greeting/index.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+function handler(req, res) {
+  res.json({ 
+    greet: 'Hello, World!' })
+}
+
+export default handler;
